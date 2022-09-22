3675d0e719a8b3822ea6958b518d12e018281026
schen <sherryc2@andrew.cmu.edu>
2022-09-21 21:36:11 -0400

    Added Edgar Allan Poe to QueryProcessor.

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -8,6 +8,11 @@ public class QueryProcessor {
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
         }
+        if (query.toLowerCase().contains("poe")) {
+            return "Edgar Allan Poe (19 January 1809 - 7 October 1849) was an " +
+                    "American poet, writer, and literary critic, widely regarded as a central figure of " +
+                    "Romanticism in the United States, and of American literature.";
+        }
         return "";
     }
 }
