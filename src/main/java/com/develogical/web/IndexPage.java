89563f416b058da0a24acab2516e9d8f09a61562
schen <sherryc2@andrew.cmu.edu>
2022-09-21 21:21:33 -0400

    Added Hello Paragraph to IndexPage.java

--- src/main/java/com/develogical/web/IndexPage.java
+++ src/main/java/com/develogical/web/IndexPage.java
@@ -7,11 +7,11 @@ public class IndexPage extends HtmlPage {
     @Override
     protected void writeContentTo(PrintWriter writer) {
         writer.println(
-                "<h1>Welcome!</h1>" +
+                "<h1>Welcome!!</h1>" +
                 "<p>Enter your query in the box below: " +
                   "<form><input type=\"text\" name=\"q\" />" +
                     "<input type=\"submit\">" +
-                  "</form>" +
+                  "</form>" + "<p>hello</p>" +
                 "</p>");
     }
     
